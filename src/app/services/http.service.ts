e0e3d6261d4d60cad0f80119f3414e68287a1c39
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2023-01-19 17:32:24 +0300

    get expenses

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Expense } from "../models/expense.model";
+
 
 @Injectable()
 export class HttpService {
-  constructor(http: HttpClient) { }
+  constructor(private http: HttpClient) { }
+
+  getAllExpenses = (): Observable<Expense> => {
+    return this.http.get('http://localhost:8000/expenses').pipe(map((result: Expense) => {
+      return result;
+    }))
+  }
 }
